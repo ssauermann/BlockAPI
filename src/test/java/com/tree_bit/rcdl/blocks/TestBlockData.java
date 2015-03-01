46565bec9f4befc2498db0eac8b6bdc9a08fde2e
S. Sauermann <SaschaSauermann@gmx.de>
2015-03-01 19:31:24 +0100

    Moved test method to other class.

--- src/test/java/com/tree_bit/rcdl/blocks/TestBlockData.java
+++ src/test/java/com/tree_bit/rcdl/blocks/TestBlockData.java
@@ -11,13 +11,6 @@ import org.junit.Test;
 public class TestBlockData {
 
     @Test
-    public void testToCount() {
-        assertEquals(2, BlockData.toCount(180, 90));
-        assertEquals(-3, BlockData.toCount(-270, 90));
-        assertEquals(4, BlockData.toCount(120, 30));
-    }
-
-    @Test
     public void testGetDataValue() {
         assertEquals(0, Repeater.getInstance(OrientationNESW.North, Delay.D1).getDataValue());
         assertEquals(5, Repeater.getInstance(OrientationNESW.East, Delay.D2).getDataValue());
