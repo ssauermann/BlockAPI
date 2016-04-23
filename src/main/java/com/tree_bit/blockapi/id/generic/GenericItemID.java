664e63952aad97047ac58e9cf08483821554151d
S. Sauermann <SaschaSauermann@gmx.de>
2016-04-23 13:06:51 +0200

    added some generic id classes

---
+++ src/main/java/com/tree_bit/blockapi/id/generic/GenericItemID.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2016 The BlockAPI authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.tree_bit.blockapi.id.generic;
+
+import com.tree_bit.blockapi.id.IItemID;
+
+import org.eclipse.jdt.annotation.NonNull;
+
+
+/**
+ *
+ */
+public class GenericItemID implements IItemID {
+
+    private final int id;
+    private final String name;
+
+    /**
+     * Creates an generic item id instance.
+     *
+     * @param id id value
+     * @param name alphabetical id
+     */
+    public GenericItemID(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int getID() {
+        return this.id;
+    }
+
+    @Override
+    public @NonNull String getAlphabeticalID() {
+        return this.name;
+    }
+
+}
