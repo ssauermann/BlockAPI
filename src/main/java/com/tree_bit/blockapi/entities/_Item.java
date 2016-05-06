509a8cd4b9ab3707e19a17e38b22572a54fe907b
S. Sauermann <SaschaSauermann@gmx.de>
2016-05-06 12:30:31 +0200

    added comments

--- src/main/java/com/tree_bit/blockapi/entities/_Item.java
+++ src/main/java/com/tree_bit/blockapi/entities/_Item.java
@@ -45,30 +45,78 @@ abstract class _Item implements NBTData {
                 .add(this.tag()).build();
     }
 
+    /**
+     * Number of items stacked in this inventory slot. Any item can be stacked,
+     * including tools, armor, and vehicles. Range is -128 to 127. Values of 1
+     * are not displayed in-game. Values below 1 are displayed in red.
+     *
+     * <p>
+     * Default: 1
+     *
+     * @return current count
+     */
     @Value.Parameter(order = 2)
     @Value.Default
     public byte count() {
         return 1;
     }
 
+    /**
+     * The inventory slot the item is in.
+     *
+     * <p>
+     * Default: 0
+     *
+     * @return current slot number
+     */
     @Value.Parameter(order = 4)
     @Value.Default
     public byte slotNr() {
         return 0;
     }
 
+    /**
+     * The data value for this item. The name "Damage" comes from when only
+     * tools used this value, now many other items use this value for other
+     * purposes. For blocks, it is the 4-bit "block data" tag that determines a
+     * variant of the block. Defaults to 0.
+     *
+     * <p>
+     * Default: 0
+     *
+     * @return current damage value
+     */
     @Value.Parameter(order = 3)
     @Value.Default
     public short damage() {
         return 0;
     }
 
+    /**
+     * Item/Block ID. If not specified, Minecraft changes the item to stone
+     * (setting ID to 1 and Damage to 0, and ignoring any existing Damage value)
+     * when loading the chunk or summoning the item.
+     *
+     * <p>
+     * Default: {@link BlockID#STONE}
+     *
+     * @return current damage value
+     */
     @Value.Parameter(order = 1)
     @Value.Default
     public String id() {
         return BlockID.STONE.getAlphabeticalID();
     }
 
+    /**
+     * Additional information about the item, discussed in the below sections.
+     * This tag is optional for most items.
+     *
+     * <p>
+     * Optional
+     *
+     * @return additional informations compound tag
+     */
     @Value.Parameter(order = 5)
     public abstract Optional<CompoundTag> tag();
 
